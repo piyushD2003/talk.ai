d2bdbdc697b81d11a87ce681f02c72ed1166a308
Piyush Dhyani <piyushdhyani7@gmail.com>
2024-11-13 17:25:16 +0530

    script tag changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,6 +7,8 @@ const Navbar = () => {
     const [key, setKey] = useState("")
     const [API, setAPI] = useState("")
     const [color, setColor] = useState("")
+    // const [Navdropdown, setNavdropdown] = useState("hidden")
+    // const [Servicedropdown, setServicedropdown] = useState("hidden")
     useEffect(() => {
         if(localStorage.getItem('Skey')){
             setAPI(localStorage.getItem('Skey'))
@@ -41,7 +43,7 @@ const Navbar = () => {
                     {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Talk.ai</span>
                 </a>
-                <button data-collapse-toggle="navbar-dropdown" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
+                <button data-collapse-toggle="navbar-dropdown" type="button" className={`inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600`} aria-controls="navbar-dropdown" aria-expanded="false" >
                     <span className="sr-only">Open main menu</span>
                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
@@ -53,7 +55,7 @@ const Navbar = () => {
                             <a href="/" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</a>
                         </li>
                         <li>
-                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Services <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" type="button" className={`flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent`}>Services <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
                             </svg></button>
 
@@ -132,7 +134,7 @@ const Navbar = () => {
                     </ul>
                 </div>
             </div>
-            <Script src="https://unpkg.com/flowbite@1.6.0/dist/flowbite.js"></Script>
+            <script src="https://unpkg.com/flowbite@1.6.0/dist/flowbite.js" async/>
         </nav>
 
     )
